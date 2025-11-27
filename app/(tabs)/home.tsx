83cb19a7b1ef7fa23074d156b496a68c9a5d395b
cjw9379 <cjw7434@gmail.com>
2025-11-27 13:53:20 +0900

    생년월일입력

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,15 +1,10 @@
 //홈 탭 화면 역할
-
-import { Link } from 'expo-router';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function Index(){
   return (
     <View style={styles.container}>
-      <Text style={styles.text}> I'm Home screen</Text>
-      <Link href="/about" style={styles.button}>//버튼
-        Go to About screen
-      </Link>
+      <Text style={styles.text}>I'm Home screen</Text>
     </View>
   );
 }
--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,9 +1,48 @@
-import { Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+//useState 생년월일 문자열 저장
 
-export default function Settings(){
+export default function SettingsScreen(){
+    const [birthday, setBirthday]=useState("");//문자열 초기화
+
     return (
-        <View>
-            <Text>Settings 화면</Text>
+        <View style={styles.container}>
+            <Text style={styles.title}>생년월일 입력하기</Text>
+            <TextInput
+                style={styles.input}
+                placeholder="YYYY-MM-DD"
+                placeholderTextColor="#587c92ff"
+                value={birthday}
+                onChangeText={setBirthday}
+            />
+            <Text style={styles.result}>입력: {birthday}</Text>
         </View>
     );
 }
+
+const styles=StyleSheet.create({
+    container: {
+        flex:1,//화면 전체 채우기
+        backgroundColor: "#15104aff",//배경색 검정
+        alignItems: "center",//가로축 가운데 정렬
+        justifyContent: "center",//세로축 가운데 정렬
+        paddingHorizontal: 20,//좌우 패딩, 화면 끝에 여백
+    },
+    title: {
+        fontSize: 20,
+        color: "#77b6c7ff",
+        marginBottom: 20,//아래쪽 여백
+    },
+    input: {
+        width: "80%",//View 너비의 80% 차지(유동적)
+        padding: 12,//안쪽 여백(입력텍스트와 경계 사이)
+        backgroundColor: "#9896cfff",
+        borderRadius: 8,//모서리 둥글게!!
+        fontSize: 16,
+        marginBottom: 20,//입련란 아래 여백
+    },
+    result: {
+        fontSize: 16,
+        color: "#fff",
+    },
+});
